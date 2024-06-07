7d16b639a69621085aaea7ed8d761146c61749d6
StefanosDepountis <147147645+StefanosDepountis@users.noreply.github.com>
2024-06-07 18:51:29 +0300

    Update definitions.h

--- src/definitions.h
+++ src/definitions.h
@@ -13,9 +13,10 @@ float TareOffeset_1=8219669; // calibration
 unsigned int time0;
 float  f(float x);
 float f_dot(float x);
-float projection(float x, float y);
-float proj_fun(float x, float e, float x_max);
-float der_proj_fun(float x, float e, float x_max);
+float projection(float x,float y,float xmax,float e)
+// float projection(float x, float y);
+// float proj_fun(float x, float e, float x_max);
+// float der_proj_fun(float x, float e, float x_max);
 float Scale2Height(float weight);
 int filter(float u);
 
