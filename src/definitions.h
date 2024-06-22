52bcc64f6b71904a11c0c82d97fa245d5740f0dd
gigantios <147148437+gigantios@users.noreply.github.com>
2024-06-22 20:14:43 +0300

    Update definitions.h

--- src/definitions.h
+++ src/definitions.h
@@ -1,15 +1,15 @@
 #include <HX711_ADC.h>
 
-HX711_ADC LoadCell_2(2,3); //HX711 1
-HX711_ADC LoadCell_1(4,5); //HX711 2
-float calibrationValue_2=717.37; // calibration value load cell 1
-float calibrationValue_1=-732.42; // calibration value load cell 2
+HX711_ADC LoadCell_2(2, 3);  //HX711 1
+HX711_ADC LoadCell_1(4, 5);  //HX711 2
+float calibrationValue_2 = 717.37;  //calibration value load cell 1
+float calibrationValue_1 = -732.42;  //calibration value load cell 2
 
-float TareOffeset_2=8516111; // calibration value load cell 1
-float TareOffeset_1=8219669; // calibration 
+float TareOffeset_2 = 8516111;  //calibration value load cell 1
+float TareOffeset_1 = 8219669;  //calibration 
 
 unsigned int time0;
-float  f(float x);
+float f(float x);
 float f_dot(float x);
 float projection(float x, float y);
 float proj_fun(float x, float e, float x_max);
@@ -17,26 +17,28 @@ float der_proj_fun(float x, float e, float x_max);
 float Scale2Height(float weight);
 int filter(float u);
 
-const int pwm=6;
-const int in1=7;
-const int in2=8;
+const int pwm = 6;
+const int in1 = 7;
+const int in2 = 8;
 
 void initPump();
 void drivePump(float speed);
-float EulerIntegrator(float loop_time,float state_dot);
+float EulerIntegrator(float loop_time, float state_dot);
 
-float u_max=14;
-// SECTION Simulation Constants
-float g=9.81;
-float k2=3;
-float k1=50000;
-float constant=1e-6;
-float gamma1=constant;
-float gamma2=constant;
-float gamma_beta=constant;
+float u_max = 14;
 
-float h2d=0.12; // in Metres
+//SECTION Simulation Constants
+float g = 9.81;
+float k2 = 3;
+float k1 = 50000;
+float constant = 1e-6;
+float gamma1 = constant;
+float gamma2 = constant;
+float gamma_beta = constant;
+
+float h2d = 0.12;  //in Metres
+
 //SECTION - Initialazation
-float a1_hat=0.1;
-float a2_hat=0.1;
-float p_hat=0.1;
+float a1_hat = 0.1;
+float a2_hat = 0.1;
+float p_hat = 0.1;
